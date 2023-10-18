fafbec503183eecfe927323deb84f9c8e73e0335
srg <srg@codeserg.io>
2023-10-18 14:40:15 -0600

    cleanup, font to _app, blog route

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+
+import { Inter } from 'next/font/google';
+import type { AppProps } from 'next/app';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={inter.className}>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
