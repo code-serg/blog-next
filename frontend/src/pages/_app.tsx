90aa7714297c72dd25cf7fa3e5c45194a09c437a
srg <srg@codeserg.io>
2023-10-19 10:07:41 -0600

    minor restructure, Head adjustments

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css';
+import Head from 'next/head';
 
 import { Inter } from 'next/font/google';
 import type { AppProps } from 'next/app';
@@ -7,10 +8,22 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={inter.className}>
-      <main>
-        <Component {...pageProps} />
-      </main>
-    </div>
+    <>
+      <Head>
+        <title>Blog - Next</title>
+        <meta
+          name="description"
+          content="Full Stack Blog with MERN + Next.js"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={inter.className}>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </>
   );
 }
--- frontend/src/pages/blog.tsx
+++ frontend/src/pages/blog.tsx
@@ -1,3 +1,16 @@
+import Head from 'next/head';
+
 export default function BlogPage() {
-  return <div>Hello New Blog!</div>;
+  return (
+    <>
+      <Head>
+        <title>Articles - Blog</title>
+        <meta
+          name="description"
+          content="Full Stack Blog with MERN + Next.js"
+        />
+      </Head>
+      <div>Hello New Blog!</div>
+    </>
+  );
 }
