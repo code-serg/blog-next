6562da07f5558f18dc392d61dd508b9a029552cf
srg <srg@codeserg.io>
2023-10-30 15:32:04 -0600

    added and configure cors and @types/cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,19 @@
 import 'dotenv/config';
 import express from 'express';
 import blogPostRoutes from './routes/blog-posts';
+import cors from 'cors';
+import env from './env';
 
 const app = express();
 
 // parse JSON bodies into JS objects
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: env.FRONTEND_URL,
+  })
+);
 
 app.use('/posts', blogPostRoutes);
 
