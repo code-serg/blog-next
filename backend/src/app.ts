6b505f29ebf3725f8496fad9260955bd225e176b
srg <srg@codeserg.io>
2023-12-06 11:47:55 -0700

    error handling for "resource not found"

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 import env from './env';
 import morgan from 'morgan';
 import errorHandler from './middleware/errorHandler';
+import createHttpError from 'http-errors';
 
 const app = express();
 
@@ -22,6 +23,8 @@ app.use(
 app.use('/uploads/featured-images', express.static('uploads/featured-images'));
 
 app.use('/posts', blogPostRoutes);
+
+app.use((req, res, next) => next(createHttpError(404, 'Resource Not found')));
 
 app.use(errorHandler);
 
