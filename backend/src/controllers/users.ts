7592865588195fc2c31e8a256621d9fc53fc2370
srg <srg@codeserg.io>
2023-12-07 14:10:48 -0700

    install bcrupt, added user controller

---
+++ backend/src/controllers/users.ts
@@ -0,0 +1,39 @@
+import { RequestHandler } from 'express';
+import createHttpError from 'http-errors';
+import bcrypt from 'bcrypt';
+import UserModel from '../models/user';
+
+interface SignUpBody {
+  username: string;
+  password: string;
+  email: string;
+}
+export const signUp: RequestHandler<unknown, unknown, SignUpBody, unknown> = async (req, res, next) => {
+  const { username, password: passwordRaw, email } = req.body;
+  try {
+    const existingUsername = await UserModel.findOne({ username })
+      .collation({ locale: 'en', strength: 2 })
+      .exec();
+
+    if (existingUsername) {
+      throw createHttpError(409, 'Username already exists');
+    }
+
+    const passwordHashed = await bcrypt.hash(passwordRaw, 10);
+
+    const result = await UserModel.create({
+      username,
+      displayName: username,
+      email,
+      password: passwordHashed,
+    });
+
+    // remove password from result before sending it back
+    const newUser = result.toObject();
+    delete newUser.password;
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
+};
