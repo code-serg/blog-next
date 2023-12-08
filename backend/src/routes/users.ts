13bdc456623d4f28ad1a0ecd57b7e09180eee819
srg <srg@codeserg.io>
2023-12-08 15:23:43 -0700

    add route for login that calls possport for authentication

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import passport from 'passport';
 import * as UserController from '../controllers/users';
 
 const router = express.Router();
 
 router.post('/signup', UserController.signUp);
+
+router.post('/login', passport.authenticate('local'), (req, res) => res.status(200).json(req.user));
 
 export default router;
