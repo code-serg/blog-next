832df5ff1a1b3b34b0f6fd0e8f598e9e4edbcdd6
srg <srg@codeserg.io>
2023-12-08 17:03:08 -0700

    added session and passport "app.use" in app.ts

--- backend/src/config/passport.ts
+++ backend/src/config/passport.ts
@@ -1,42 +1,37 @@
 import passport from 'passport';
 import { Strategy as LocalStrategy } from 'passport-local';
+import UserModel from '../models/user';
 import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
-import UserModel from '../models/user';
 
-// serializeUser determines which data of the user object should be stored in the session in the database.
 passport.serializeUser((user, cb) => {
   cb(null, user._id);
 });
 
-// deserializeUser will call with the unique id provided by serializeUser
-// to retrieve the user object.
 passport.deserializeUser((userId: string, cb) => {
-  // commonly seen is to return the user object from the database
-  // but here we are opting to just return the object with the user id
   cb(null, { _id: new mongoose.Types.ObjectId(userId) });
 });
 
 passport.use(
-  // local strategy is used for username/password based authentication
   new LocalStrategy(async (username, password, cb) => {
     try {
-      // must use 'select' explicitly to get fields that are not returned/selected by default - see the model
       const existingUser = await UserModel.findOne({ username }).select('+email +password').exec();
 
       if (!existingUser || !existingUser.password) {
-        return cb(null, false, { message: 'No such username / password.' });
+        return cb(null, false);
       }
 
       const passwordMatch = await bcrypt.compare(password, existingUser.password);
 
       if (!passwordMatch) {
-        return cb(null, false, { message: 'No such username / password.' });
+        return cb(null, false);
       }
 
       const user = existingUser.toObject();
+
       delete user.password;
-      return cb(null, user, { message: 'Logged In Successfully' });
+
+      return cb(null, user);
     } catch (error) {
       cb(error);
     }
