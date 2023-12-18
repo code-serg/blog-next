f5ed42b83614a5c75817721a50b4c5750551f371
SRG <srg@codeserg.io>
2023-12-18 11:46:57 -0700

    signup form: added fields including a passwordinput component

--- frontend/src/components/auth/SignupModal.tsx
+++ frontend/src/components/auth/SignupModal.tsx
@@ -1,4 +1,14 @@
-import { Modal } from 'react-bootstrap';
+import { Form, Modal } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import * as UsersApi from '@/network/api/users';
+import FormInputField from '../form/FormInputField';
+import PasswordInputField from '../form/PasswordInputField';
+
+interface SignupFormData {
+  username: string;
+  email: string;
+  password: string;
+}
 
 interface SignupModalProps {
   onDismiss: () => void;
@@ -6,14 +16,58 @@ interface SignupModalProps {
 }
 
 export default function SignupModal({ onDismiss, onLoginInstead }: SignupModalProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignupFormData>();
+
+  async function onSubmit(credentials: SignupFormData) {
+    try {
+      const newUser = await UsersApi.signup(credentials);
+      alert(JSON.stringify(newUser));
+    } catch (error) {
+      console.error(error);
+      alert(error);
+    }
+  }
+
   return (
     <Modal
       show
       onHide={onDismiss}
+      centered
     >
       <Modal.Header closeButton>
         <Modal.Title>Sign Up</Modal.Title>
       </Modal.Header>
+      <Modal.Body>
+        <Form
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+        >
+          <FormInputField
+            label="Username"
+            register={register('username', { required: 'Required' })}
+            placeholder="Enter Username"
+            maxLength={50}
+            error={errors.username}
+          />
+          <FormInputField
+            label="Email"
+            type="email"
+            register={register('email', { required: 'Required' })}
+            placeholder="Enter Email"
+            maxLength={254}
+            error={errors.email}
+          />
+          <PasswordInputField
+            label="Password"
+            register={register('password', { required: 'Required' })}
+            error={errors.password}
+          />
+        </Form>
+      </Modal.Body>
     </Modal>
   );
 }
