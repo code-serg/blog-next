20a22889c7b10dccb0762f9e98c4607f94cde52e
SRG <srg@codeserg.io>
2024-01-29 12:18:13 -0700

    minor edit"

--- backend/src/utils/validation.ts
+++ backend/src/utils/validation.ts
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 
 export const imageFileSchema = yup
   .mixed<Express.Multer.File>()
-  .test('valid-image', 'Unsupported file format - not a valid image', async (file) => {
+  .test('valid-image', 'Not a valid image', async (file) => {
     if (!file) return true; // allow empty values - image is optional
 
     const result = await validateBufferMIMEType(file.buffer, {
