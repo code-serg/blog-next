e3181f76c4bff2c3d7287b064af91ec0eda9a061
SRG <srg@codeserg.io>
2024-01-29 09:17:40 -0700

    building validation for blog input incl file

---
+++ backend/src/utils/validation.ts
@@ -0,0 +1,14 @@
+import { validateBufferMIMEType } from 'validate-image-type';
+import * as yup from 'yup';
+
+export const imageFileSchema = yup
+  .mixed<Express.Multer.File>()
+  .test('valid-image', 'Unsupported file format - not a valid image', async (file) => {
+    if (!file) return true; // allow empty values - image is optional
+
+    const result = await validateBufferMIMEType(file.buffer, {
+      allowMimeTypes: ['image/jpeg', 'image/png'],
+    });
+
+    return result.ok;
+  });
---
+++ backend/src/validation/blog-post.ts
@@ -0,0 +1,18 @@
+import * as yup from 'yup';
+import { imageFileSchema } from '../utils/validation';
+
+const blogPostBodySchema = yup.object({
+  title: yup.string().required().max(105),
+  slug: yup
+    .string()
+    .required()
+    .max(105)
+    .matches(/^[a-zA-Z0-9_-]*$/),
+  summary: yup.string().required().max(300),
+  body: yup.string().required(),
+});
+
+export const createBlogPostSchema = yup.object({
+  body: blogPostBodySchema,
+  file: imageFileSchema.required('Featured image is required'),
+});
