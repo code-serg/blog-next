bcd05ab9d9cfda6d8a38a0ee43c8352f16a99b0e
SRG <srg@codeserg.io>
2024-01-29 17:52:47 -0700

    Add request schema validation to user signup route - now using Yup

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import passport from 'passport';
 import * as UserController from '../controllers/users';
 import requiresAuth from '../middleware/requiresAuth';
+import validateRequestSchema from '../middleware/validareRequestSchema';
+import { signupSchema } from '../validation/users';
 
 const router = express.Router();
 
-router.post('/signup', UserController.signUp);
+router.post('/signup', validateRequestSchema(signupSchema), UserController.signUp);
 
 router.post('/login', passport.authenticate('local'), (req, res) => res.status(200).json(req.user));
 
